a07690568553afb898b196f5226a9909e5aaa7e5
taradepan <taradepan1313@gmail.com>
2024-10-18 15:52:38 +0000

    .

---
+++ main.py
@@ -0,0 +1,40 @@
+from swarm import Swarm, Agent
+from swarm.repl import run_demo_loop
+import json
+from utils import get_flight_prices, search, get_current_date_time, pretty_print_messages
+client = Swarm()
+
+agent = Agent(
+    name="Agent",
+    instructions="""Your task is to check if you got the correct response for the user query.
+    In case you don't know the departure_id or arrival_id, you can search for the Airport IATA code. 
+    Search for the cheapest flight prices based on the user query and return the results. BEFORE passing to flight agent check the current date and time (if Needed).
+    If user has any more queries, answer them.
+    for get_flight_prices, type can be 1, 2, 3.
+    1 - Round trip (default)
+    2 - One way
+    3 - Multi-city
+    """,
+    functions=[get_flight_prices, search, get_current_date_time],
+)
+
+
+print("Starting Swarm CLI 🐝")
+context_variables = None
+stream = False
+debug = False
+messages = []
+while True:
+    user_input = input("\033[90mUser\033[0m: ")
+    messages.append({"role": "user", "content": user_input})
+
+    response = client.run(
+        agent=agent,
+        messages=messages,
+    )
+
+    pretty_print_messages(response.messages)
+
+    messages.extend(response.messages)
+    agent = response.agent
+    print("Agent: ", response.agent)
---
+++ utils.py
@@ -0,0 +1,68 @@
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+from serpapi import GoogleSearch
+import json
+
+load_dotenv()
+
+def get_flight_prices(departure_id, arrival_id, outbound_date, return_date=None, type="1"):
+    params = {
+        "api_key": os.getenv("SERP_API_KEY"),
+        "engine": "google_flights",
+        "hl": "en",
+        "gl": "us",
+        "type": type,
+        "departure_id": departure_id,
+        "arrival_id": arrival_id,
+        "outbound_date": outbound_date,
+        "return_date": return_date,
+        "currency": "INR"
+    }
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    return results
+# print(get_flight_prices("BLR","MAA","2024-10-19"))
+
+def search(Search_input):
+    params = {
+    "api_key": os.getenv("SERP_API_KEY"),
+    "engine": "google",
+    "q": Search_input,
+    "google_domain": "google.com",
+    "hl": "en"
+    }
+    print(params)
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    results = results.get("answer_box").get("answer") if results.get("answer_box") else results.get("related_questions")
+    print(results)      
+    return results
+
+
+def get_current_date_time():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def pretty_print_messages(messages) -> None:
+    for message in messages:
+        if message["role"] != "assistant":
+            continue
+
+        # print agent name in blue
+        print(f"\033[94m{message['sender']}\033[0m:", end=" ")
+
+        # print response, if any
+        if message["content"]:
+            print(message["content"])
+
+        # print tool calls in purple, if any
+        tool_calls = message.get("tool_calls") or []
+        if len(tool_calls) > 1:
+            print()
+        for tool_call in tool_calls:
+            f = tool_call["function"]
+            name, args = f["name"], f["arguments"]
+            arg_str = json.dumps(json.loads(args)).replace(":", "=")
+            print(f"\033[95m{name}\033[0m({arg_str[1:-1]})")
+
